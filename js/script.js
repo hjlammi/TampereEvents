54dc75f7bffe23fb3a3bdaa6975e08fd7aa23ac9
Heidi Lammi-Mihaljov <heidi.lammi@mihaljov.info>
2017-05-07 12:36:40 +0300

    Show all the upcoming events

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
   $.ajax({
     type: 'GET',
     dataType: 'json',
-    url: "https://visittampere.fi/api/search?type=event&limit=10",
+    url: "https://visittampere.fi/api/search?type=event&limit=100",
     success: naytaTiedot,
     error: function() {
       alert( "Tiedon noutaminen ei onnistunut" );
@@ -11,6 +11,7 @@
 }());
 
 function naytaTiedot(data) {
+  var otsikko, kuva, kuva_alt, kuvaus, paikkakunta, osoite, info, ajankohta;
   $.each(data, function(index, tapahtuma) {
     otsikko = tapahtuma.title;
     kuva = tapahtuma.image.src;
@@ -21,25 +22,38 @@ function naytaTiedot(data) {
     info = tapahtuma.contact_info.link;
 
     console.log(tapahtuma);
-    if (tapahtuma.start_datetime === null) {
-      if (tapahtuma.times[0] === undefined) {
-      } else {
-        tapahtuma_aika = tapahtuma.times[0].start_datetime;
-      }
-    } else {
-      tapahtuma_aika = tapahtuma.start_datetime;
-    }
+    ajankohta = annaTapahtumaAika(tapahtuma);
 
-    nykyinenHetki = new Date();
-    nykyHetkiMS = Date.parse(nykyinenHetki);
-    if (nykyHetkiMS < tapahtuma_aika) {
-      ajankohta = tapahtuma_aika;
-    } else {
-      ajankohta = null;
+    if (ajankohta !== null) {
+      lisaaTapahtuma(otsikko, kuva, kuva_alt, kuvaus, paikkakunta, osoite, info, ajankohta);
     }
-
   });
+}
 
+// Metodi saa parametrina tapahtuman ja antaa tapahtuman ajankohdan paluuarvona.
+function annaTapahtumaAika(tapahtuma) {
+  if (tapahtuma.start_datetime === null) {
+    if (tapahtuma.times[0] === undefined) {
+      return null;
+    } else {
+      tapahtuma_aika = tapahtuma.times[0].start_datetime;
+    }
+  } else {
+    tapahtuma_aika = tapahtuma.start_datetime;
+  }
+
+  nykyinenHetki = new Date();
+  nykyHetkiMS = Date.parse(nykyinenHetki);
+  console.log(nykyHetkiMS);
+  if (nykyHetkiMS < tapahtuma_aika) {
+    return tapahtuma_aika;
+  } else {
+    return null;
+  }
+}
+
+
+function lisaaTapahtuma(otsikko, kuva, kuva_alt, kuvaus, paikkakunta, osoite, info, ajankohta) {
   var tapahtumaElementti = $('#tapahtuma').clone();
   tapahtumaElementti.children('h2').html(otsikko + ' <small>' + osoite + ', ' + paikkakunta + '</small>');
   tapahtumaElementti.find('.kuvaus').text(kuvaus);
